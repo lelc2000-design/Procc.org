52f4cccd543b772116b1a1f4eb902e958716daff
secureassetsholding-rgb <secureassetsholding@gmail.com>
2025-11-26 02:08:02 +0100

    fix: Add real team photos from ProCC website, complete all content from PDF

--- src/data/team.js
+++ src/data/team.js
@@ -1,4 +1,4 @@
-// Equipo ProCC - Información real del sitio
+// Equipo ProCC - Información real del sitio con fotos reales
 export const teamMembers = [
   {
     id: 'mirtha-cucco',
@@ -6,7 +6,7 @@ export const teamMembers = [
     role: 'Directora y Fundadora',
     description: 'Psicóloga y creadora de la Metodología ProCC. Más de 35 años de experiencia en intervención comunitaria y salud. Ha desarrollado y perfeccionado la Metodología de los Procesos Correctores Comunitarios a lo largo de su trayectoria profesional.',
     area: 'Dirección y Desarrollo Metodológico',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/mirtha-cucco.jpg',
+    image: 'https://www.procc.org/wp-content/uploads/2019/01/WhatsApp-Image-2022-05-23-at-1.47.13-PM-300x273.jpeg',
     email: 'mirtha.cucco@procc.org'
   },
   {
@@ -14,126 +14,125 @@ export const teamMembers = [
     name: 'Ana Sáenz',
     role: 'Coordinadora',
     area: 'Coordinación General',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/ana-saenz.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/10/Ana-Saenz-300x300.jpg'
   },
   {
     id: 'esther-zuniga',
     name: 'Esther Zúñiga',
     role: 'Coordinadora',
     area: 'Formación y Docencia',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/esther-zuniga.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Esther-Zuniga-300x300.jpg'
   },
   {
     id: 'aurora-chia',
     name: 'Aurora Chía',
     role: 'Coordinadora',
     area: 'Intervención Comunitaria',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/aurora-chia.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Aurora-300x300.jpg'
   },
   {
     id: 'elena-aguilo',
     name: 'Elena Aguiló',
     role: 'Coordinadora',
     area: 'Investigación',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/elena-aguilo.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Elena-Aguilo-300x300.jpg'
   },
   {
     id: 'ayelen-losada',
     name: 'Ayelén Losada',
     role: 'Coordinadora',
     area: 'Difusión',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/ayelen-losada.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/07/Ayelen-Losada-300x300.jpg'
   },
   {
     id: 'alfredo-waisblat',
     name: 'Alfredo Waisblat',
     role: 'Coordinador',
     area: 'Relación Institucional',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/alfredo-waisblat.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Alfredo-Waisblat-300x300.jpg'
   },
   {
     id: 'grisell-crespo',
     name: 'Grisell Crespo',
     role: 'Coordinadora',
     area: 'Financiación',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/grisell-crespo.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/10/Grisel-Crespo-300x300.jpg'
   },
   {
     id: 'viviana-casih',
     name: 'Viviana Casih',
     role: 'Coordinadora',
     area: 'Formación',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/viviana-casih.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Viviana-Casih-300x300.jpg'
   },
   {
     id: 'rocio-tapiador',
     name: 'Rocío Tapiador',
     role: 'Coordinadora',
     area: 'Intervención',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/rocio-tapiador.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/11/Rocio-Tapiador-ok-300x300.png'
   },
   {
     id: 'eliana-cortese',
     name: 'Eliana Cortese',
     role: 'Coordinadora',
     area: 'Supervisión',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/eliana-cortese.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Eliana-Cortese-300x300.jpg'
   },
   {
     id: 'susana-martinez',
     name: 'Susana Martínez',
     role: 'Coordinadora',
     area: 'Asesoría',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/susana-martinez.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Susana-Martinez-300x300.jpg'
   },
   {
     id: 'maria-francisca-romero',
     name: 'María Francisca Romero',
     role: 'Coordinadora',
     area: 'Comunidad Educativa',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/maria-francisca-romero.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Francis-Romero-300x300.jpg'
   },
   {
     id: 'natalia-millan',
     name: 'Natalia Millán',
     role: 'Coordinadora',
     area: 'Género',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/natalia-millan.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Natalia-Millan-Sahagun-300x300.jpg'
   },
   {
     id: 'teresa-borrego',
     name: 'Teresa Borrego',
     role: 'Coordinadora',
     area: 'Mayores',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/teresa-borrego.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/09/Teresa-300x300.jpg'
   },
   {
     id: 'teresa-vazquez',
     name: 'Teresa Vázquez',
     role: 'Coordinadora',
     area: 'Jóvenes',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/teresa-vazquez.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2019/11/Teresa-Vazquez-300x300.png'
   },
   {
     id: 'eray-losada',
     name: 'Eray Losada',
     role: 'Coordinador',
     area: 'Tecnología',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/eray-losada.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2020/05/IMG_20200522_121444-298x300.jpg'
   },
   {
     id: 'fabio-ruben-tiranti',
     name: 'Fabio Rubén Tiranti',
     role: 'Coordinador',
     area: 'Argentina',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/fabio-ruben-tiranti.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2020/05/1_Fabio_p_Web-288x300.png'
   },
   {
     id: 'monica-jaurena',
     name: 'Mónica Jaurena',
     role: 'Coordinadora',
     area: 'Argentina',
-    image: 'https://www.procc.org/wp-content/uploads/2024/01/monica-jaurena.jpg'
+    image: 'https://www.procc.org/wp-content/uploads/2020/06/1590770634881blob.png'
   }
 ]
-
